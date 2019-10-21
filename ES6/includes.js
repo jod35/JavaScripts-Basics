078fbfc3d3830dc1569108db65161d5ca221132c
jod35 <jodestrevin@gmail.com>
2019-10-21 16:01:09 +0300

    for of loop

---
+++ ES6/includes.js
@@ -0,0 +1,13 @@
+//old js
+var myarray=[1,2,34,4,5,6,7];
+//to find out if a certain value exists,
+t= myarray.indexOf(0);
+console.log(t);
+
+//-1
+
+//ES6
+let myarray1=[1,2,3,4,5,6,7,8];
+console.log(myarray1.includes(8));
+//returns true
+
---
+++ ES6/let and const.js
@@ -0,0 +1,16 @@
+// if (false){
+//     var example =5;
+// }
+// console.log(example);
+//returns undefined
+
+// if (true) {
+//     let example1=16;
+// }
+// console.log(example1);
+
+// this returns an error because let works in its block scope
+
+const pi=3.14;
+pi=3;
+// const cannot change to any other value
