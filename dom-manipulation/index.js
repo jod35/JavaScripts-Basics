e39ea8c58e668862720bf55b854c1dfcae120ca5
jod35 <jodestrevin@gmail.com>
2019-10-23 22:58:50 +0300

    getElementByTagName

--- dom-manipulation/index.js
+++ dom-manipulation/index.js
@@ -1,4 +1,11 @@
-const stuff =document.getElementById('stuff');
-stuff.style.backgroundColor="red";
-alert(typeof stuff);
+// const li =document.getElementsByTagName('li');
+// for (let index = 0; index < li.length; index++) {
+//     alert(typeof li[index]);
+// }
+// alert (document.getElementsByTagName("*").length);
 
+var stuff =document.getElementById('stuff');
+
+var items=stuff.getElementsByTagName("*");
+
+alert(items.length); //returns 4 items in the list
