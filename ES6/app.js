b524aaa3093d4801f3b85550b035d4029e66aec4
jod35 <jodestrevin@gmail.com>
2019-11-04 00:22:35 +0300

    block scoping

---
+++ ES6/app.js
@@ -0,0 +1,8 @@
+var foo =[1,2,3];
+
+var obj = {
+    foo
+}
+
+console.log(obj.foo);
+///[1,2,3]
---
+++ ES6/scope_declations.js
@@ -0,0 +1,10 @@
+let a =2;
+
+const show_a=()=>{
+    a=3;
+    console.log(a);
+};
+
+show_a();
+// returns 3 since its inside the block's scope
+
