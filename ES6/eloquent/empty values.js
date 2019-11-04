fc2a385e2da6e107c65206b92cc26dfddfd266ca
jod35 <jodestrevin@gmail.com>
2019-11-04 16:56:31 +0300

    empty values

---
+++ ES6/eloquent/empty values.js
@@ -0,0 +1,18 @@
+// console.log(null);
+// //null
+// console.log(null * 3);
+//0   in this case null turns to a zero
+
+//automatic type conversion
+
+console.log("5" -1);
+//4 beacuse the string 5 turns to an integer
+console.log("5" +1);
+//51 because the integer 1 turns to  a string.
+
+console.log("five"*2);
+//returns Nan
+
+
+
+
---
+++ ES6/eloquent/logical operators.js
@@ -0,0 +1,12 @@
+console.log(true && true);
+//true
+console.log(true && false);
+//false
+console.log(false && false);
+//false
+console.log(true && false);
+//false
+
+
+
+
