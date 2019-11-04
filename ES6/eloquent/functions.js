e8e89bc6d49cb039a1034495b707cef1bb79e4f2
jod35 <jodestrevin@gmail.com>
2019-11-04 18:07:32 +0300

    functions

---
+++ ES6/eloquent/functions.js
@@ -0,0 +1,28 @@
+//most of the default values in javascript environment are functions
+//ffnctions help us to reuse code 
+// forexample
+
+function addNumbers(x,y){
+    return x+y;
+
+}
+
+console.log(addNumbers(3,4));
+
+// function convertToCelcius(F){
+//     return (F-32)*(5/9);
+// };
+
+// console.log(convertToCelcius(212));
+
+//100
+
+function convertToFarhenHeit(C){
+    return ((9/5) *C)+32;
+}
+
+console.log(convertToFarhenHeit(100));
+//212
+
+
+
