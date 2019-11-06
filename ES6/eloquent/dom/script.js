174a0ec5dfcf20259e83cb73a2d35f3d8f751f8c
jod35 <jodestrevin@gmail.com>
2019-11-06 10:40:31 +0300

    shopping list DOM manipulation

---
+++ ES6/eloquent/dom/script.js
@@ -0,0 +1,3 @@
+const text=document.getElementById('text');
+const addbtn =document.getElementById('add');
+const my_ul=document.getElementById('myul');
---
+++ ES6/eloquent/for loops.js
@@ -0,0 +1,42 @@
+//all even numbers or equal to  24
+
+let count =0;
+
+for (i=0;i<24;i++){
+    count++;
+    if(count%2==0){
+        console.log(count);
+        
+    }
+}
+// 2;
+// 4;
+// 6;
+// 8;
+// 10;
+// 12;
+// 14;
+// 16;
+// 18;
+// 20;
+// 22;
+// 24;
+
+//looping over an array
+
+let myarray=[1,2,3,4,5,6,6,7];
+
+for(i=0;i<myarray.length;i++){
+   console.log(i);
+   
+}
+
+// 1;   prints the indices
+// 2;
+// 3;
+// 4;
+// 5;
+// 6;
+// 7;
+// 8;
+
